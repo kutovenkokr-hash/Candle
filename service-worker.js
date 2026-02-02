ab41fd01b53801b77ca3ca667493e4b8724f5585
kutovenkokr-hash <kutovenkokr@gmail.com>
2026-02-02 08:34:48 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "candle-pwa-v1";
+const CACHE_NAME = "candle-pwa-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -18,3 +18,4 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
